e35e1b40092853846438908b5bfae2f4e6e700ba
Yesica <yazmn05@gmail.com>
2026-06-16 21:25:14 -0500

    Reto día 5: Iterar por opciones de menú superior y validar path url completado

--- tests/navigation.spec.ts
+++ tests/navigation.spec.ts
@@ -74,3 +74,104 @@ test('Navigate through the left panel', async ({ page }) => {
 
     }
 });
+
+test('Check all the qualification links', async ({ page }) => {
+
+    const expectPages = [
+        {
+            menu: 'Skills',
+            url: '/web/index.php/admin/viewSkills'
+        },
+        {
+            menu: 'Education',
+            url: '/web/index.php/admin/viewEducation'
+        },
+        {
+            menu: 'Licenses',
+            url: '/web/index.php/admin/viewLicenses'
+        },
+        {
+            menu: 'Languages',
+            url: '/web/index.php/admin/viewLanguages'
+        },
+        {
+            menu: 'Memberships',
+            url: '/web/index.php/admin/membership'
+        }
+    ];
+
+    await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+    await page.getByRole('textbox', { name: 'Username' }).fill('Admin');
+    await page.getByRole('textbox', { name: 'Password' }).fill('admin123');
+    await page.getByRole('button', { name: 'Login' }).click();
+
+    await expect(page.getByRole('link', { name: 'Admin' })).toBeVisible();
+
+    await page.getByRole('link', { name: 'Admin' }).click();
+
+    await page.getByRole('navigation', { name: 'Topbar Menu' }).getByText('Qualifications').click();
+     
+    const qualificationOptions = page.getByRole('menu').locator('li');
+
+    for(let expectPage of expectPages) {
+
+        const menuOption = qualificationOptions.filter({hasText: expectPage.menu});
+        await menuOption.click();
+        await expect(page).toHaveURL(new RegExp(expectPage.url));
+
+        await page.getByRole('navigation', { name: 'Topbar Menu' }).getByText('Qualifications').click();
+    }
+
+
+});
+
+
+test('Check all the job links', async ({ page }) => {
+
+    const expectPages = [
+        {
+            menu: 'Job Titles',
+            url: '/web/index.php/admin/viewJobTitleList'
+        },
+        {
+            menu: 'Pay Grades',
+            url: '/web/index.php/admin/viewPayGrades'
+        },
+        {
+            menu: 'Employment Status',
+            url: '/web/index.php/admin/employmentStatus'
+        },
+        {
+            menu: 'Job Categories',
+            url: '/web/index.php/admin/jobCategory'
+        },
+        {
+            menu: 'Work Shifts',
+            url: '/web/index.php/admin/workShift'
+        }
+    ];
+
+    await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+    await page.getByRole('textbox', { name: 'Username' }).fill('Admin');
+    await page.getByRole('textbox', { name: 'Password' }).fill('admin123');
+    await page.getByRole('button', { name: 'Login' }).click();
+
+    await expect(page.getByRole('link', { name: 'Admin' })).toBeVisible();
+
+    await page.getByRole('link', { name: 'Admin' }).click();
+
+    await page.getByRole('navigation', { name: 'Topbar Menu' }).getByText('Job').click();
+    
+    const jobOptions = page.getByRole('menu').locator('li');
+
+    for(let expectPage of expectPages) {
+
+        const menuOption = jobOptions.filter({hasText: expectPage.menu});
+        await menuOption.click();
+        await expect(page).toHaveURL(new RegExp(expectPage.url));
+
+        await page.getByRole('navigation', { name: 'Topbar Menu' }).getByText('Job').click();
+    }
+
+
+});
